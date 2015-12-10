07cad22e64d2830464dbe92c97dd43d7f00ea70b
Hamzah Aly <hzhaly93@gmail.com>
2015-12-10 15:23:02 -0800

    fixed UI

--- js/app.js
+++ js/app.js
@@ -47,14 +47,22 @@ states.MainMenu.prototype = {
         this.game.load.image('x', 'assets/img/drops/x.png');
         this.game.load.image('y', 'assets/img/drops/y.png');
         this.game.load.image('z', 'assets/img/drops/z.png');
-        this.game.load.image('start', 'assets/img/start.png');
-        this.game.load.image('leaderboard', 'assets/img/leaderboard.png');
+        this.game.load.image('start', 'assets/img/clouds4nightz/wordcloudbuttons-01.png');
+        this.game.load.image('leaderboard', 'assets/img/clouds4nightz/wordcloudbuttons-02.png');
+        this.game.load.image('playagain', 'assets/img/clouds4nightz/wordcloudbuttons-03.png');
+        this.game.load.image('clouds', 'assets/img/clouds4nightz/wordcloudclouds-02.png');
         this.game.load.image('replay', 'assets/img/replay.png');
         this.game.load.audio('theme', 'assets/audio/theme.mp3');
         this.game.load.audio('buttonClick', 'assets/audio/buttonClick.mp3');
         this.game.load.audio('keyPress', 'assets/audio/keyPress.mp3');
         this.game.load.audio('correct', 'assets/audio/correct.wav');
         this.game.load.audio('wrong', 'assets/audio/wrong.wav');
+        this.game.load.image('grass1',"assets/img/grass4dayz/wordcloudgrass2-01.png");
+        this.game.load.image('grass2',"assets/img/grass4dayz/wordcloudgrass3-01.png");
+        this.game.load.image('grass3',"assets/img/grass4dayz/wordcloudgrass4-01.png");
+        this.game.load.image('grass4',"assets/img/grass4dayz/wordcloudgrass6-01.png");
+        this.game.load.image('grass5',"assets/img/grass4dayz/wordcloudgrass-01.png");
+
         console.log('loaded sprites');
     },
     create: function() {
@@ -72,11 +80,11 @@ states.MainMenu.prototype = {
         //buttonClickFX.addMarker('replay', 0, 5);
 
         //Start the game by clicking this button
-        var startButton = this.game.add.button(game.world.centerX, game.world.centerY, "start", this.startGame, this);
+        var startButton = this.game.add.button(game.world.centerX, game.world.centerY - 25, "start", this.startGame, this);
         startButton.anchor.setTo(0.5, 0.5);
         startButton.scale.set(0.2, 0.2);
 
-        var leaderButton = this.game.add.button(game.world.centerX, game.world.centerY + 85, "leaderboard", this.LeaderBoard, this);
+        var leaderButton = this.game.add.button(game.world.centerX, game.world.centerY + 100, "leaderboard", this.LeaderBoard, this);
         leaderButton.anchor.setTo(0.5, 0.5);
         leaderButton.scale.set(0.2, 0.2);
         //makeButton('leaderboard', game.world.centerX - 85, game.world.centerY + 60);
@@ -97,7 +105,7 @@ states.LeaderBoard.prototype = {
     create: function() {
         background = game.add.tileSprite(0, 0, 650, 700, "background");
     }
-}
+};
 
 states.GameOver.prototype = {
     create: function() {
@@ -110,11 +118,11 @@ states.GameOver.prototype = {
         yourScore.anchor.setTo(0.5, 0.5);
 
         //Start the game by clicking this button
-        var startButton = this.game.add.button(game.world.centerX, game.world.centerY + 100, "start", this.startGame, this);
+        var startButton = this.game.add.button(game.world.centerX / 2, game.world.centerY + 100, "playagain", this.startGame, this);
         startButton.anchor.setTo(0.5, 0.5);
         startButton.scale.set(0.2, 0.2);
 
-        var leaderBoard = this.game.add.button(game.world.centerX, game.world.centerY + 200, "leaderboard", this.LeaderBoard, this);
+        var leaderBoard = this.game.add.button(game.world.centerX * 1.5, game.world.centerY + 100, "leaderboard", this.LeaderBoard, this);
         leaderBoard.anchor.setTo(0.5, 0.5);
         leaderBoard.scale.set(0.2, 0.2);
     },
@@ -210,7 +218,7 @@ function create() {
 
     //Adds gravity to the drops
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.arcade.gravity.y = 5;
+    game.physics.arcade.gravity.y = 20;
 
     //creates the drops group that Phaser implements
     drops = game.add.group();
